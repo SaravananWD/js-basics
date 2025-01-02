aa320973fd5e0411d3e5aede3144746e1f66de61
Saravanan Devaraj <saravanankcti@gmail.com>
2025-01-02 23:42:43 +0530

    change mobile adaptability

--- nav bar/script.js
+++ nav bar/script.js
@@ -4,10 +4,32 @@ let navBtn = document.querySelector(".navbutton");
 let navBar = document.querySelector(".navbar");
 
 navBtn.addEventListener("click", () => {
-  if (navBar.classList.contains("show-links")) {
-    navBar.classList.remove("show-links");
-  } else {
-    navBar.classList.add("show-links");
-  }
-  //navBar.classList.toggle("show-links");
+  navBar.classList.toggle("show-links");
 });
+
+// add emulate features
+
+let emulateBtn = document.getElementById("emulate-mobile-view");
+let emulateStatus = document.getElementById("emulatebtnstatus");
+
+emulateBtn.addEventListener("click", () => {
+  if (!document.querySelector(".emulateStyleSheet")) {
+    let link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.className = "emulateStyleSheet";
+    link.href = "project_style_emulate.css";
+    document.getElementsByTagName("head")[0].appendChild(link);
+    displayEmulateMsg("Done.", "#118811");
+  } else {
+    displayEmulateMsg(
+      "Alert: Already in resposive view. Refresh to reset.",
+      "#881111"
+    );
+  }
+});
+
+function displayEmulateMsg(msg, bgcolor) {
+  emulateStatus.style.display = "inline-block";
+  emulateStatus.style.backgroundColor = bgcolor;
+  emulateStatus.textContent = msg;
+}
