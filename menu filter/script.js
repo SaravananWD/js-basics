d01b4b092f1ab31964ada19b2f74c57f5145c0af
Saravanan Devaraj <saravanankcti@gmail.com>
2025-01-04 20:46:32 +0530

    start new project

---
+++ menu filter/script.js
@@ -0,0 +1,407 @@
+/* Project related JavaScript */
+
+const menu = [
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0000_wynand-van-poortvliet-4AmyOdXZAQc-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0001_timothy-muza-4F42IqLRjf4-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0002_ray-hennessy-xUUZcpQlqpM-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0003_randall-ruiz-LVnJlyfa7Zk-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0004_mikhail-vasilyev-IFxjDdqK_0U-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0005_matthew-henry-2Ts5HnA67k8-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0006_john-cobb-mk2USqDQE5E-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0007_jessica-weiller-kZ8dyUT0h30-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0008_jan-jakub-nanista-xqjO-lx39B4-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0009_geranimo-bKhETeDV1WM-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "animals",
+    src: "thumbnail__0010_gary-bendig-6GMq7AGxNbE-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0000_thought-catalog-9aOswReDKPo-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0001_maja-petric-vGQ49l9I4EE-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0002_luke-michael-1cWZgnBhZRs-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0003_joseph-gonzalez-QaGDmf5tMiE-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0004_joseph-gonzalez-fdlZBWIP0aM-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0005_food-photographer-jennifer-pallian-dZKiXR9FYcM-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0006_charlesdeluvio-D-vDQMTfAAU-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0007_calum-lewis-8Nc_oQsc2qQ-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0008_bon-vivant-qom5MPOER-I-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0009_arturrro-GdTLaWamFHw-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "food",
+    src: "thumbnail__0010_alex-munsell-Yr4n8O_3UPc-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0000_wietse-jongsma-klBnmXpY1DA-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0001_tron-le-aM3KBX6twTI-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0002_quan-jing-A5yzj2mjTMQ-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0003_fausto-garcia-menendez-MkIY8Br0J3E-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0004_benjamin-chambon-hCqiMLrnw8I-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0005_benjamin-chambon-8I0-dsJYg1g-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0006_ashley-nicole-113BuKkNGt8-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0007_ajay-karpur-OXTUa-5PSO8-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "people",
+    src: "thumbnail__0008_abenezer-shewaga-5fJECoPYoC0-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0014_husqy-_official-6laKkLBZn2I-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0015_harley-davidson-Wy3sdT0B9Fo-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0013_jorgen-hendriksen-i3BGqO7m8k4-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0011_pedram-normohamadian-ts26KpllwY0-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0012_kai-pilger-Kia9F9pkA2U-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0010_phyo-min-rz1WKqoijjY-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0008_taras-chernus-Algo40PI79M-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0009_sara-kurfess-bth720DH6GI-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0007_thisguyshoots-htssRj0ZIUA-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0005_wahyu-setiawan-qWjfWJLlnQc-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0006_tuan-anh-tran-M74UY3ozHPY-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0004_aditya-chinchure-cCXcHIzLbfc-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0002_austin-neill-_uHFBjtwZ1E-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0003_andrew-pons-cLHPacdtpSY-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0001_austin-park-b5R5COcD9lk-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+  {
+    id: "",
+    title: "",
+    category: "vehicles",
+    src: "thumbnail_0000_harley-davidson-4ixHdlcROPI-unsplash.jpg",
+    description:
+      "Lorem ipsum dolor sit amet. Est quia esse ut quidem omnis sed quas magnam!",
+  },
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  displayMenu(menu);
+  let buttonContainer = document.querySelector(".btn-container");
+  buttonContainer.innerHTML = menu
+    .reduce((categories, item) => {
+      if (!categories.includes(item.category)) {
+        categories.push(item.category);
+      }
+    }, "all")
+    .map((category) => {
+      return `<button class="btn-s filter-btn">${category}</button>`;
+    });
+});
+
+function displayMenu(menuSelected) {
+  let menuContainer = document.querySelector(".menu-container");
+  menuContainer.innerHTML = menuSelected
+    .map((item) => {
+      return `<article class="menu-item"><img src="./img/${item.category}/${item.src}"
+class="menu-item-img"><div class="menu-item-info"><p>${item.description}</p></div></article>`;
+    })
+    .join("");
+}
