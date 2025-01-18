37bb9c91da4245e3edd26e71434a65c6e9906bcd
Saravanan Devaraj <saravanankcti@gmail.com>
2025-01-18 14:38:44 +0530

    final test

--- video background/script.js
+++ video background/script.js
@@ -1,3 +1,21 @@
 /* Project related JavaScript */
 
 let video = document.querySelector(".video-container");
+let btn = document.querySelector(".switch-btn");
+let playBtn = document.querySelector(".play");
+let pauseBtn = document.querySelector(".pause");
+
+playBtn.addEventListener("click", () => {
+  btn.classList.toggle("slide");
+  video.play();
+});
+pauseBtn.addEventListener("click", () => {
+  btn.classList.toggle("slide");
+  video.pause();
+});
+
+let preloader = document.getElementById("preloader");
+
+window.addEventListener("load", () => {
+  preloader.classList.add("hide-preloader");
+});
