62a25c854594db2cd2a002189152cd9611798f0c
Saravanan Devaraj <saravanankcti@gmail.com>
2025-01-19 07:23:10 +0530

    layout done

--- smooth scroll/script-full.js
+++ smooth scroll/script-full.js
@@ -1,19 +1,25 @@
 /* Project related JavaScript */
 
 // navbar dynamically find & set height
+const navBar = document.querySelector(".navbar");
 const navBtn = document.querySelector(".navbutton");
-const navLinks = document.querySelector(".navlinks");
+const links = document.querySelector(".navlinks");
 const linksContainer = document.querySelector(".links-container");
 
 navBtn.addEventListener("click", () => {
+  // class toggle for animation effects (optional)
+  navBar.classList.toggle("show-links");
+
   // find height
-  const navHeight = navLinks.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
   let containerHeight = linksContainer.getBoundingClientRect().height;
 
   // set height
   if (containerHeight === 0) {
-    linksContainer.style.height = `${navHeight}px`;
+    linksContainer.style.height = `${linksHeight}px`;
+    navBtn.style.transform = "rotate(-90deg)";
   } else {
     linksContainer.style.height = 0;
+    navBtn.style.transform = "rotate(0)";
   }
 });
