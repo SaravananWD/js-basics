49272bcf0cf9cb2375679fdd2e47f58d97a29176
Saravanan Devaraj <saravanankcti@gmail.com>
2025-01-19 13:08:58 +0530

    project check

--- smooth scroll/script-full.js
+++ smooth scroll/script-full.js
@@ -1,10 +1,13 @@
 /* Project related JavaScript */
 
-// navbar dynamically find & set height
+// navbar
 const navBar = document.querySelector(".navbar");
+const navContainer = document.querySelector(".nav-container");
 const navBtn = document.querySelector(".navbutton");
 const links = document.querySelector(".navlinks");
 const linksContainer = document.querySelector(".links-container");
+
+console.log();
 
 navBtn.addEventListener("click", () => {
   // class toggle for animation effects (optional)
@@ -17,9 +20,58 @@ navBtn.addEventListener("click", () => {
   // set height
   if (containerHeight === 0) {
     linksContainer.style.height = `${linksHeight}px`;
-    navBtn.style.transform = "rotate(-90deg)";
   } else {
     linksContainer.style.height = 0;
-    navBtn.style.transform = "rotate(0)";
   }
+});
+
+// check scroll height
+
+window.addEventListener("scroll", () => {
+  let scrollHeight = window.scrollY;
+  let navBarHeight = navBar.getBoundingClientRect().height;
+  let topLink = document.querySelector(".top");
+
+  if (scrollHeight > navBarHeight) {
+    navContainer.classList.add("fixed-nav");
+  } else {
+    navContainer.classList.remove("fixed-nav");
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+// smooth scrolling
+
+let scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    let id = e.currentTarget.getAttribute("href").slice(1);
+    let elem = document.getElementById(id);
+
+    let navBarHeight = navBar.getBoundingClientRect().height;
+    let isFixed = navContainer.classList.contains("fixed-nav");
+    let position = elem.offsetTop;
+    console.log("navBarHeight: " + navBarHeight);
+    console.log("position: " + position);
+    console.log("scrollY: " + window.scrollY);
+
+    if (!isFixed) {
+      position = position - navBarHeight;
+    }
+
+    window.scrollTo({
+      top: position - navBarHeight,
+      left: 0,
+      behavior: "smooth",
+    });
+    linksContainer.style.height = 0;
+    navBar.classList.remove("show-links");
+  });
 });
