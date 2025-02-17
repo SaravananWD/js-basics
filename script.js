896e5add38a3700353b78d93bed3177561b5f362
Saravanan Devaraj <saravanankcti@gmail.com>
2025-02-17 20:50:39 +0530

    update index

---
+++ script.js
@@ -0,0 +1,57 @@
+const projects = [
+  {
+    name: "Color Flipper",
+    url: "./01_color flipper/index.html",
+  },
+  {
+    name: "Counter",
+    url: "./02_counter/index.html",
+  },
+  {
+    name: "Content Navigator",
+    url: "./03_content navigator/index.html",
+  },
+  {
+    name: "Nav Bar",
+    url: "./04_nav bar/index.html",
+  },
+  {
+    name: "Side Bar",
+    url: "./05_side bar/index.html",
+  },
+  {
+    name: "Questions Panel",
+    url: "./06_questions panel/index.html",
+  },
+  {
+    name: "Menu Filter",
+    url: "./07_menu filter/index.html",
+  },
+  {
+    name: "Video Background",
+    url: "./08_video background/index.html",
+  },
+  {
+    name: "Smooth Scroll",
+    url: "./09_smooth scroll/index-full.html",
+  },
+  {
+    name: "Tabs",
+    url: "./10_tabs/index.html",
+  },
+  {
+    name: "Countdown",
+    url: "./11_countdown/index.html",
+  },
+  {
+    name: "To Do List",
+    url: "./12_to do list/index.html",
+  },
+];
+const projectsContainer = document.querySelector(".projects-container");
+
+let count = 1;
+projects.forEach((project) => {
+  projectsContainer.innerHTML += `<a class="project-info" href="${project.url}"><span>${count}</span>${project.name}</a>`;
+  count++;
+});
